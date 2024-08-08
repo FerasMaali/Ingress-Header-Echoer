b631f7541940fbc820ec9e03ca68876a03a91df6
Feras Maali <feras@quizplus.com>
2024-08-08 10:37:56 +0300

    match all routes

--- index.js
+++ index.js
@@ -2,16 +2,12 @@
 
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
-// Define a route that returns the request headers
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.json(req.headers);
 });
 
-// Set the port for the server
-const PORT = process.env.PORT || 3000;
-
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
